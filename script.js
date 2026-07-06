1a329cc05f273660bedef48eec047e1407ecac3f
jjjjjjjjj594 <kd921936@gmail.com>
2026-07-06 18:07:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,23 +4,43 @@ const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-let player = {
+const player = {
   x: 100,
   y: 100,
   size: 30,
-  color: "blue"
+  speed: 5
 };
 
-function drawPlayer() {
-  ctx.fillStyle = player.color;
+const keys = {};
+
+window.addEventListener("keydown", (e) => {
+  keys[e.key] = true;
+});
+
+window.addEventListener("keyup", (e) => {
+  keys[e.key] = false;
+});
+
+function update() {
+  if (keys["ArrowUp"]) player.y -= player.speed;
+  if (keys["ArrowDown"]) player.y += player.speed;
+  if (keys["ArrowLeft"]) player.x -= player.speed;
+  if (keys["ArrowRight"]) player.x += player.speed;
+}
+
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  ctx.fillStyle = "#5cb85c";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  ctx.fillStyle = "blue";
   ctx.fillRect(player.x, player.y, player.size, player.size);
 }
 
 function gameLoop() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-  drawPlayer();
-
+  update();
+  draw();
   requestAnimationFrame(gameLoop);
 }
 
